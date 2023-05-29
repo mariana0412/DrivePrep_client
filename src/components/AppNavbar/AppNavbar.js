741cf1cd442a1e404ec4d02f6940604ef13f2cc2
Mariana Piz <marianapiz0412@gmail.com>
2023-05-29 16:23:17 +0300

    Create AppNavbar

---
+++ src/components/AppNavbar/AppNavbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './AppNavbar.css';
+import homeIcon from '../../images/homeIcon.svg';
+import login from '../../images/login.svg';
+import MyButton from "../UI/button/MyButton";
+
+const AppNavbar = () => {
+
+    return (
+        <nav className="navbar">
+            <ul className="navbar-list">
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/">
+                        <img src={homeIcon} className="homeImg" alt="home"/>
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/tests">Тести</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/rules">Правила</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/study-materials">
+                        <div className="multi-line-label">
+                            <span>Навчальні</span><span>матеріали</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/fines">Штрафи</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/laws">Закони</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/alcotester">Алкотестер</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="navbar-link" to="/number-identifier">
+                        <div className="multi-line-label">
+                            <span>Визначник</span><span>номерів</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className="navbar-item navbar-item-right">
+                    <Link className="navbar-link" to="/profile">
+                        <MyButton style={{ margin: '5px' }}>Профіль</MyButton>
+                    </Link>
+                    <Link className="navbar-link" to="/login">
+                        <img src={login} className="loginImg" alt="login"/>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default AppNavbar;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import AppNavbar from './AppNavbar/AppNavbar';
+
+const Home = () => {
+
+    return (
+        <div>
+            <AppNavbar />
+        </div>
+    );
+};
+
+export default Home;
