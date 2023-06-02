3676be3590fd3f79fb2b02d86188b07a4a0cced1
Mariana Piz <marianapiz0412@gmail.com>
2023-06-02 21:08:02 +0300

    Implement Fines page

---
+++ src/components/Fines/Fines.js
@@ -0,0 +1,16 @@
+import FinesTable from "./FinesTable";
+import './Fines.css';
+import AppNavbar from "../AppNavbar/AppNavbar";
+import React from "react";
+
+const Fines = () => {
+    return (
+        <div>
+            <AppNavbar/>
+            <FinesTable/>
+        </div>
+    )
+
+}
+
+export default Fines;
