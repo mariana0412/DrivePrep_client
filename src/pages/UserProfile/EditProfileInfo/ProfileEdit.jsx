0b62b569d27dd827cdb74eaa06b9c1a3d6209b53
Mariana Piz <marianapiz0412@gmail.com>
2023-06-09 20:38:17 +0300

    Check name, surname and email before updating

--- src/pages/UserProfile/EditProfileInfo/ProfileEdit.jsx
+++ src/pages/UserProfile/EditProfileInfo/ProfileEdit.jsx
@@ -3,13 +3,13 @@ import MyInput from "../../../components/UI/input/MyInput";
 import classes from "./ProfileEdit.module.css";
 import MyButton from "../../../components/UI/button/MyButton";
 import MySelect from "../../../components/UI/MySelect/MySelect";
+import {emailFormatIsValid, emailIsRu, nameAndSurnameAreNotEmpty} from "../../../utils/userDataValidation";
 
 const ProfileEdit = ({ categories, user }) => {
     const [newSurname, setNewSurname] = useState('');
     const [newName, setNewName] = useState('');
     const [newCategoryId, setNewCategoryId] = useState('');
     const [newEmail, setNewEmail] = useState('');
-
 
     useEffect(() => {
         if (user) {
@@ -29,6 +29,9 @@ const ProfileEdit = ({ categories, user }) => {
             email: newEmail || user.email,
         };
 
+        if(!nameIsValid() || !emailIsValid())
+            return;
+
         fetch(`http://localhost:8080/users/${user.id}`, {
             method: 'PUT',
             headers: {
@@ -37,16 +40,37 @@ const ProfileEdit = ({ categories, user }) => {
             body: JSON.stringify(profileUpdatePayload),
         })
             .then(response => {
-                if (response.ok) {
-                   alert('Profile data saved successfully');
-                   
-                } else {
-                    alert('Failed to update profile data');
-                }
+                if (response.ok)
+                   alert('Інформація змінена успішно!');
+                else if(response.status === 400)
+                    alert('Ця електронна адреса вже використовується!');
+                else
+                    alert("Не вийшло змінити інформацію...")
             })
             .catch(error => {
-                console.error('Failed to update profile data:', error);
+                console.error('Не вийшло змінити інформацію:', error);
             });
+    };
+
+    const emailIsValid = () => {
+        const validEmailFormat = emailFormatIsValid(newEmail);
+        const ruEmail = emailIsRu(newEmail);
+
+        if(validEmailFormat && !ruEmail)
+            return true;
+        else if(!validEmailFormat)
+            alert("Будь-ласка, введіть коректну електронну адресу.");
+        else if(ruEmail)
+            alert("Система не підтримує використання РОСІЙСЬКИХ поштових адрес");
+        return false;
+    }
+
+    const nameIsValid = () => {
+        if(!nameAndSurnameAreNotEmpty(newName, newSurname)) {
+            alert("Ім'я та прізвище є обов'язковими.");
+            return false;
+        }
+        return true;
     };
 
     return (
